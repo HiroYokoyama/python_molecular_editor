8ff3700baa0c29cd06c210f47f8274d5c2b9a982
HiroYokoyama <58853434+HiroYokoyama@users.noreply.github.com>
2025-11-17 17:22:04 +0900

    Add files via upload

---
+++ moleditpy/src/moleditpy/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+MoleditPy — A Python-based molecular editing software
+
+Author: Hiromichi Yokoyama
+License: Apache-2.0 license
+Repo: https://github.com/HiroYokoyama/python_molecular_editor
+DOI 10.5281/zenodo.17268532
+"""
+
+
+import sys
+import ctypes
+
+from PyQt6.QtWidgets import QApplication
+
+from modules.main_window import MainWindow
+
+def main():
+    # --- Windows タスクバーアイコンのための追加処理 ---
+    if sys.platform == 'win32':
+        myappid = 'hyoko.moleditpy.1.0' # アプリケーション固有のID（任意）
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+    app = QApplication(sys.argv)
+    file_path = sys.argv[1] if len(sys.argv) > 1 else None
+    window = MainWindow(initial_file=file_path)
+    window.show()
+    sys.exit(app.exec())
