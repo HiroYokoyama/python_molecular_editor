1012048d92fb7149c908e16a8a543b222aa1e0c6
HiroYokoyama <58853434+HiroYokoyama@users.noreply.github.com>
2025-11-17 17:22:04 +0900

    Add files via upload

---
+++ moleditpy/src/moleditpy/__main__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+MoleditPy — A Python-based molecular editing software
+
+Author: Hiromichi Yokoyama
+License: Apache-2.0 license
+Repo: https://github.com/HiroYokoyama/python_molecular_editor
+DOI 10.5281/zenodo.17268532
+"""
+
+
+print("-----------------------------------------------------")
+print("MoleditPy — A Python-based molecular editing software")
+print("-----------------------------------------------------\n")
+
+from main import main
+
+# --- Application Execution ---
+if __name__ == '__main__':
+    main()
+
