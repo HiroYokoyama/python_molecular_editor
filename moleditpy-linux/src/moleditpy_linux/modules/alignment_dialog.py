c41fd190945998445b86442174015def6c6b18f3
HiroYokoyama <58853434+HiroYokoyama@users.noreply.github.com>
2025-11-20 12:32:25 +0900

    Add files via upload

--- moleditpy-linux/src/moleditpy_linux/modules/alignment_dialog.py
+++ moleditpy-linux/src/moleditpy_linux/modules/alignment_dialog.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+MoleditPy — A Python-based molecular editing software
+
+Author: Hiromichi Yokoyama
+License: GPL-3.0 license
+Repo: https://github.com/HiroYokoyama/python_molecular_editor
+DOI 10.5281/zenodo.17268532
+"""
+
 from PyQt6.QtWidgets import (
     QDialog, QVBoxLayout, QLabel, QHBoxLayout, QPushButton
 )
--- moleditpy-linux/src/moleditpy_linux/modules/main_window_view_loaders.py
+++ moleditpy-linux/src/moleditpy_linux/modules/main_window_view_loaders.py
@@ -2,6 +2,15 @@
 # -*- coding: utf-8 -*-
 
 """
+MoleditPy — A Python-based molecular editing software
+
+Author: Hiromichi Yokoyama
+License: GPL-3.0 license
+Repo: https://github.com/HiroYokoyama/python_molecular_editor
+DOI 10.5281/zenodo.17268532
+"""
+
+"""
 main_window_view_loaders.py
 MainWindow (main_window.py) から分離されたモジュール
 機能クラス: MainWindowViewLoaders
