6d2aa3c5b18aca01a0f349dad4ba9588e85df608
HiroYokoyama <58853434+HiroYokoyama@users.noreply.github.com>
2025-11-21 12:35:26 +0900

    Add files via upload

--- moleditpy-linux/src/moleditpy_linux/modules/settings_dialog.py
+++ moleditpy-linux/src/moleditpy_linux/modules/settings_dialog.py
@@ -7,7 +7,7 @@ MoleditPy — A Python-based molecular editing software
 Author: Hiromichi Yokoyama
 License: GPL-3.0 license
 Repo: https://github.com/HiroYokoyama/python_molecular_editor
-DOI 10.5281/zenodo.17268532
+DOI: 10.5281/zenodo.17268532
 """
 
 from PyQt6.QtWidgets import (
